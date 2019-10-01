89e44176ea4c51bc4a056e4319cf4f8b41c60770
Ivo Meixner <natiwastaken@gmail.com>
2019-10-01 17:12:45 +0200

    Add protobuf generic marshal / unmarshal code

--- main.go
+++ main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"io/ioutil"
+	"log"
 
+	"github.com/golang/protobuf/proto"
+)
+
+func main() {
+	// TODO
+}
+
+func marshal(file string, msg proto.Message) {
+	out, err := proto.Marshal(msg)
+
+	if err != nil {
+		log.Fatalln("Failed to encode address book:", err)
+	}
+
+	if err := ioutil.WriteFile(file, out, 0644); err != nil {
+		log.Fatalln("Failed to write address book:", err)
+	}
+}
+
+func unmarshal(file string, msg proto.Message) {
+	in, err := ioutil.ReadFile(file)
+
+	if err != nil {
+		log.Fatalln("Error reading file:", err)
+	}
+
+	if err := proto.Unmarshal(in, msg); err != nil {
+		log.Fatalln("Failed to parse address book:", err)
+	}
 }
